90bd2d4ff9638bda085e84a29a17b7a6c3880669
Zurie123bs <zurikokartvelishvili@gmail.com>
2024-05-22 19:44:03 +0400

    third commit

---
+++ level 1/homework/homework1.py
@@ -0,0 +1,10 @@
+name = "Zura"
+surname = "Qartvelishvili"
+age = "13"
+
+
+
+print(" me var" + " " + name + " " + surname + " da me var" + " "+ age + " " + "wlis")
+
+
+
--- levell 1/homework/homework1.py
+++
@@ -1,2 +0,0 @@
-print("zura qartvelishvili")
-print("this is day 1")
