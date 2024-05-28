f03cd293f5269f69c0337a3afc3424d84084bd0d
Zurie123bs <zurikokartvelishvili@gmail.com>
2024-05-28 22:26:22 +0400

    fourth commit

---
+++ level 3/homework/homework 3.py
@@ -0,0 +1,34 @@
+name="zura"
+last_name="qartvelishvili"
+age="13"
+hobby="kalatburtis tamashi,legoebis da pazlebis awyoba"
+qountry="saqartvelo"
+print(name + " " + last_name + " " + age + " " + hobby + " " + qountry)
+
+# შექმენით ცვლადები გადაეცით მნიშვნელობები და შემდგომ დაამატეთ ერთმანეთზე
+
+num=46
+num2=27
+num3=91
+num4=12
+num5=36
+num6=65
+num7=98
+num8=123
+num9=54
+num10=142
+print(num + num2 + num3)
+
+#გააკეთეთ 10 მაგალითი ყველა მათემატიკურ ოპერატორებზე.+,-,*,//,/,**
+
+print(num2 + num6)
+print(num5 + num7)
+print(num10 - num9)
+print(num8 - num5)
+print(num * num3)
+print(num4 / num2 + num10)
+print(num6 // num3 -num9)
+print(num7 ** num)
+print(num2 * num9 -num5)
+print(num6 + num - num9 * num10 / num4 ** num5 // num8)
+
